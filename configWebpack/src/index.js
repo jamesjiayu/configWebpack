9968a7d32d44e348605d20af4eed888ae3b8564b
jamesjiayu <jamesjiayu@users.noreply.github.com>
2023-03-30 10:12:09 -0500

    1st loader scss babel

---
+++ configWebpack/src/index.js
@@ -0,0 +1,12 @@
+import './index.scss'
+
+import React from 'react'
+
+export default function index () {
+  return (
+    <div className="james"></div>
+  )
+}
+
+console.log('test, webpack')
+const sum = () => { }
---
+++ configWebpack/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // 将 JS 字符串生成为 style 节点
+          'style-loader',
+          // 将 CSS 转化成 CommonJS 模块
+          'css-loader',
+          // 将 Sass 编译成 CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', "@babel/preset-react"]
+          }
+        }
+      }
+    ],
+  },
+}
